b4fe241e1d585bd40e045e049a2bbcd47798d966
Huy Khuong <scottkhuong@gmail.com>
2022-02-21 21:54:23 +0700

    fix brainshop

--- commands/bot.js
+++ commands/bot.js
@@ -5,8 +5,9 @@ module.exports = {
   aliases: ['b'],
   run: async (client, message, args) => {
     const res = await axios.get(
-      `http://api.brainshop.ai/get?bid=163979&key=gQsYFGyg6onbcKEw&uid=1&msg=${encodeURIComponent(message.content)}`
+      `http://api.brainshop.ai/get?bid=163979&key=gQsYFGyg6onbcKEw&uid=1&msg=${encodeURIComponent(args.join(' '))}`
     )
+    console.log(args.join(' '))
     message.channel.send(res.data.cnt)
   }
 }
